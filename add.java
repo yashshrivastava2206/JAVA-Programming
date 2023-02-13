c3fc4aece46ace5cd9385abcf107f580d7f9c7e4
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-02-13 17:42:09 +0530

    Add to numbers
    WAP to add two numbers.(without input)

---
+++ add.java
@@ -0,0 +1,11 @@
+//WAP to add two numbers.
+public class Main {
+    public static void main(String[] args){
+        int a,b,c;
+        a=10;
+        b=20;
+        c= a+b;
+        System.out.println("a:  "+a+"\nb:  "+b);
+        System.out.println("Addition of no. is : "+c);
+    }
+}
