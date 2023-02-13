4dbea45019c763bc1b2e23226881426fce8bc6b9
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-02-13 19:18:26 +0530

    Create DetectNumInt.java

---
+++ DetectNumInt.java
@@ -0,0 +1,11 @@
+//Write a JAVA program to detect weather a number entered by the user is integer or not.
+import java.util.Scanner;
+
+public class DetectNumInt {
+    public static void main(String[] args){
+        System.out.print("Enter Something :");
+        Scanner scan = new Scanner(System.in);
+        Boolean a= scan.hasNextInt();
+        System.out.println(a);
+    }
+}
