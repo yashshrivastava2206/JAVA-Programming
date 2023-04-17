2411ecb79de33e60c496f30014a54db5470f5e28
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-04-17 22:02:18 +0530

    Add files via upload

---
+++ Fundamentals Of JAVA/AddTwoNum.java
@@ -0,0 +1,58 @@
+
+public class AddTwoNum {
+
+	public static void main(String[] args) {
+		
+		
+		
+		// SWITCH() Case introduced in Java Programming
+		
+		int num1 =1;
+		int num2 =7;
+		int sum = num1+num2;
+		String str="Yash Shrivastava";
+		System.out.println(str.length());
+		System.out.println("Sum : "+sum);
+//		String value="SUNDAY";
+//		String result="";
+//		
+//		
+//		
+//		//Traditional Java
+//		
+//		switch(value) {
+//		case "SUNDAY","SATURDAY":
+//			System.out.println("Holiday");
+//			result ="Holiday";
+//			break;
+//		case "MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY":
+//			System.out.println("Working");
+//			result="Working";
+//			break;
+//		default:
+//			result="Not a Day";
+//		}
+//		System.out.println(result);
+//		
+//		
+//		
+//		//Introduced In Java-12 And implemented in JAVA-14
+//		
+//		result=switch(value) {
+//		case "SUNDAY","SATURDAY": yield "Holiday";
+//		case "MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY": yield "Working";
+//		default:
+//			yield"Not a Day";
+//		};
+//		System.out.println(result);
+//		
+//		
+//		result=switch(value) {
+//		case "SUNDAY","SATURDAY"->"Holiday";
+//		case "MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY"-> "Working";
+//		default->"Not a Day";
+//		};
+//		System.out.println(result);
+
+	}
+}
