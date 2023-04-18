d309cc9efd7d68db3c5dbaa4ebf5c6df54014cce
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-04-18 20:09:05 +0530

    Add files via upload

---
+++ Fundamentals Of JAVA/FibonacciSeries.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter nth number of fibonacci Series ");
+        int n= sc.nextInt();
+        int num1=0,num2=1;
+        if(n==1)
+    }
+}
