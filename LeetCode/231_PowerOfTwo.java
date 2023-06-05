815defb6a10ebe574155f1d6791f581cc588b402
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-06-05 21:19:46 +0530

    Create 231_PowerOfTwo.java
    Given an integer n, return true if it is a power of two. Otherwise, return false.
    
    An integer n is a power of two, if there exists an integer x such that n == 2x.

---
+++ LeetCode/231_PowerOfTwo.java
@@ -0,0 +1,21 @@
+/*
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+*/
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        int pow=1;
+        long rem=-1;
+        if(n==1)
+        return true;
+        while(n!=rem && n>rem){
+            rem=(long)Math.pow(2,pow);
+            pow++;
+        }
+        if(rem==n)
+           {return true;}
+
+        return false;
+    }
+}
