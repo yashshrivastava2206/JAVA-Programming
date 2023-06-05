7de17cf58e41e961bf12e9bdfa7c3e4c89a22abb
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-06-05 21:16:05 +0530

    268. Missing Number
    Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.

---
+++ LeetCode/268_MissingNumber.java
@@ -0,0 +1,17 @@
+/*
+Given an array nums containing n distinct numbers in the range [0, n], 
+return the only number in the range that is missing from the array.
+
+*/
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=j)
+            return j;
+            j++;
+        }
+        return j;
+    }
+}
