b1aa05a2e674a8ff8422311d679fa5b3b30b1a3f
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-06-14 23:08:06 +0530

    Create SWING_Ex1.java

---
+++ SWING/SWING_Ex1.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class SWING_Ex1 {
+    public static void main(String[] args) {
+        JFrame f= new JFrame();
+
+        f.setSize(1000,400);
+        f.setLocation(200,200);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
