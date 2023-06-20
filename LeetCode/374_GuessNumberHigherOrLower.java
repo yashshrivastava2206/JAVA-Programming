74921b77b4eb5396891138caac60c88afd2a6ef3
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-06-20 21:20:29 +0530

    374. Guess Number Higher Or Lower
    We are playing the Guess Game. The game is as follows:
    
    I pick a number from 1 to n. You have to guess which number I picked.
    
    Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
    
    You call a pre-defined API int guess(int num), which returns three possible results:
    
    -1: Your guess is higher than the number I picked (i.e. num > pick).
    1: Your guess is lower than the number I picked (i.e. num < pick).
    0: your guess is equal to the number I picked (i.e. num == pick).
    Return the number that I picked.

---
+++ LeetCode/374_GuessNumberHigherOrLower.java
@@ -0,0 +1,41 @@
+/*
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+*/
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int low=0;
+        int high=n;
+        int mid=0;
+        while(low<=high){
+            mid=low +(high-low)/2;
+            if(-1==guess(mid))
+            high=mid-1;
+            
+            else if(1==guess(mid))
+            low=mid+1;
+            else if(0==guess(mid))
+            return mid;
+        }
+        return mid;
+    }
+}
