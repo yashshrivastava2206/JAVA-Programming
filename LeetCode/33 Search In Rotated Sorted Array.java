0dad82f5eed17f9f0b8cca97782962bf776ee005
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-08-08 10:00:37 +0530

    Create 33 Search In Rotated Sorted Array.java
    There is an integer array nums sorted in ascending order (with distinct values).
    
    Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
    
    Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
    
    You must write an algorithm with O(log n) runtime complexity.

---
+++ LeetCode/33 Search In Rotated Sorted Array.java
@@ -0,0 +1,25 @@
+/*
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+class Solution {
+    public int search(int[] nums, int target) {
+        int size=nums.length-1;
+        if(nums[0]==target)
+        return 0;
+        for(int i=0;i<=size;i++,size--){
+            if(nums[i]==target || nums[size]==target){
+                if(nums[i]==target)
+                return i;
+                else
+                return size;
+            }
+        }
+        return -1;
+    }
+}
