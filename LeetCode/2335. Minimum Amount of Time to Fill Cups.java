d0dc2ecdf79db2f21bc054afc17e355505bc7d2f
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-09-27 07:23:42 +0530

    Create 2335. Minimum Amount of Time to Fill Cups.java

---
+++ LeetCode/2335. Minimum Amount of Time to Fill Cups.java
@@ -0,0 +1,21 @@
+/*
+You have a water dispenser that can dispense cold, warm, and hot water. Every second, you can either fill up 2 cups with different types of water, or 1 cup of any type of water.
+
+You are given a 0-indexed integer array amount of length 3 where amount[0], amount[1], and amount[2] denote the number of cold, warm, and hot water cups you need to fill respectively. Return the minimum number of seconds needed to fill up all the cups.
+
+ 
+*/
+class Solution {
+    public int fillCups(int[] amount) {
+        int length=amount.length;
+        int count=0;
+        Arrays.sort(amount);
+        while(amount[2]!=0){
+            count++;
+            amount[2]-=1;
+            amount[1]-=1;
+            Arrays.sort(amount);
+        }
+        return count;
+    }
+}
